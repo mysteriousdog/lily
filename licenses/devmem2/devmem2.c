b2741aff45c9cfeb2dc9b9b7ee1ae19157807710
mysteriousdog <3076777685@qq.com>
2021-11-23 10:49:06 +0800

    lic

---
+++ licenses/devmem2/devmem2.c
@@ -0,0 +1,38 @@
+/*
+ * devmem2.c: Simple program to read/write from/to any location in memory.
+ *
+ *  Copyright (C) 2000, Jan-Derk Bakker (J.D.Bakker@its.tudelft.nl)
+ *
+ *
+ * This software has been developed for the LART computing board
+ * (http://www.lart.tudelft.nl/). The development has been sponsored by
+ * the Mobile MultiMedia Communications (http://www.mmc.tudelft.nl/)
+ * and Ubiquitous Communications (http://www.ubicom.tudelft.nl/)
+ * projects.
+ *
+ * The author can be reached at:
+ *
+ *  Jan-Derk Bakker
+ *  Information and Communication Theory Group
+ *  Faculty of Information Technology and Systems
+ *  Delft University of Technology
+ *  P.O. Box 5031
+ *  2600 GA Delft
+ *  The Netherlands
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
---
+++ licenses/numactl/README.md
@@ -0,0 +1,14 @@
+Otherwise you will get spurious failures in the non-strict policies (prefered,
+interleave.)
+
+See the manpages [`numactl.8`](https://linux.die.net/man/8/numactl) and
+[`numa.3`](https://linux.die.net/man/3/numa) for details.
+
+# License, Copyrights, Acknowledgements
+
+`numactl` and the demo programs are under the GNU General Public License, v.2.
+
+`libnuma` is under the GNU Lesser General Public License, v2.1.
+
+The manpages are under the same license as the Linux manpages (see the files.)
+
